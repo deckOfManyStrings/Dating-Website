f7f19ca6bc1e625406da7553ed24a35546d22e97
Bobby Kingsada <bkingsada@greenriver.edu>
2018-01-31 00:45:01 -0500

    adding the interest page

---
+++ pages/Interest.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kingsada, bobby
+ * Date: 1/29/2018
+ * Time: 9:17 PM
+ */
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+          crossorigin="anonymous">
+
+    <title>Sign up!</title>
+</head>
+<body>
+
+<div>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item active" aria-current="page">Dating Website</li>
+        </ol>
+    </nav>
+</div>
+
+<div class="container border border-light rounded">
+    <h1>Interest</h1>
+    <div class="container">
+        <form>
+            <h6>In-door Interest</h6>
+            <div class="container">
+                <div class="form-group row">
+                    <div class="col-sm-1"></div>
+                    <div class="form-check form-check-inline col-sm-2">
+                        <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                        <label class="form-check-label" for="inlineCheckbox1">tv</label>
+                    </div>
+                    <div class="form-check form-check-inline col-sm-2">
+                        <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+                        <label class="form-check-label" for="inlineCheckbox2">movies</label>
+                    </div>
+                    <div class="form-check form-check-inline col-sm-2">
+                        <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3">
+                        <label class="form-check-label" for="inlineCheckbox3">cooking</label>
+                    </div>
+                    <div class="form-check form-check-inline col-sm-2">
+                        <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option4">
+                        <label class="form-check-label" for="inlineCheckbox3">board games</label>
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <div class="col-sm-1"></div>
+                    <div class="form-check form-check-inline col-sm-2">
+                        <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                        <label class="form-check-label" for="inlineCheckbox1">puzzels</label>
+                    </div>
+                    <div class="form-check form-check-inline col-sm-2">
+                        <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+                        <label class="form-check-label" for="inlineCheckbox2">reading</label>
+                    </div>
+                    <div class="form-check form-check-inline col-sm-2">
+                        <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3">
+                        <label class="form-check-label" for="inlineCheckbox3">playing cards</label>
+                    </div>
+                    <div class="form-check form-check-inline col-sm-2">
+                        <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option4">
+                        <label class="form-check-label" for="inlineCheckbox3">video games</label>
+                    </div>
+                </div>
+            </div>
+
+            <h6>Out-door Interest</h6>
+            <div class="container">
+                <div class="form-group row">
+                    <div class="col-sm-1"></div>
+                    <div class="form-check form-check-inline col-sm-2">
+                        <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                        <label class="form-check-label" for="inlineCheckbox1">hiking</label>
+                    </div>
+                    <div class="form-check form-check-inline col-sm-2">
+                        <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+                        <label class="form-check-label" for="inlineCheckbox2">biking</label>
+                    </div>
+                    <div class="form-check form-check-inline col-sm-2">
+                        <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3">
+                        <label class="form-check-label" for="inlineCheckbox3">swimming</label>
+                    </div>
+                    <div class="form-check form-check-inline col-sm-2">
+                        <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option4">
+                        <label class="form-check-label" for="inlineCheckbox3">collecting</label>
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <div class="col-sm-1"></div>
+                    <div class="form-check form-check-inline col-sm-2">
+                        <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                        <label class="form-check-label" for="inlineCheckbox1">walking</label>
+                    </div>
+                    <div class="form-check form-check-inline col-sm-2">
+                        <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+                        <label class="form-check-label" for="inlineCheckbox2">climbing</label>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <p align="right">
+        <button type="button" class="btn btn-primary">Next></button>
+    </p>
+</div>
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+</body>
+</html>
